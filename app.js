52811da090c18e8d2f392a81db3108f09a0fcf90
Waqar-Akhtar <akhtarwaqar384@gmail.com>
2022-10-17 18:19:17 +0500

    changing

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use('/', require('./Routes/contactus'))
 
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static("client/build"))
+    const path = require('path')
+    app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
 
 app.listen(port, ()=>{
