593fffc4329937d55c42cb2006fcb782776cae10
Waqar-Akhtar <akhtarwaqar384@gmail.com>
2022-10-17 21:50:34 +0500

    change occur

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ ConnectToMongoose();
 
 app.use('/', require('./Routes/contactus'))
 
-
-
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static("client/build"))
     const path = require('path')
     app.get('*',(req, res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
 
